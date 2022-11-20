c1f7258b24a70f38d0eef01ce65ce8026b104db0
Dhananjay-security <114823844+Dhananjay-security@users.noreply.github.com>
2022-11-20 14:56:07 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,29 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #1076CC ;"><b>TSF BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+     <center> <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <button><a class="nav-link" href="index.php" style="color:dark blue ;"><b>Home</b></a></button>
+              </li>
+              <li class="nav-item">
+                <button> <a class="nav-link" href="createuser.php" style="color :dark blue ;"><b>Create User</b></a></button>
+              </li>
+              <li class="nav-item">
+               <button>  <a class="nav-link" href="transfermoney.php" style="color :dark blue;"><b>Transfer Money</b></a></button>
+              </li>
+              <li class="nav-item">
+                <button> <a class="nav-link" href="transactionhistory.php" style="color :dark blue ;"><b>Transaction History</b></a></button>
+              </li>
+              <li class="nav-item">
+                <button> <a class="nav-link" href="https://www.thesparksfoundationsingapore.org/" style="color :dark blue ;"><b>Contact Us</b></a></button>
+              </li>
+              <!-- <li class="nav-item">
+                <button><a href="contact.php">Contact Us</a></li>
+            <li class="item"><a href="https://www.thesparksfoundationsingapore.org/">TSF</a></li> -->
+          </div>
+		  </center>
+       </nav>
